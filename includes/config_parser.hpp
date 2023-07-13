cabb371b39b3b7b858c09dbf536f47f736328059
LucasIde <lucaside000@gmail.com>
2023-07-13 20:53:21 +0200

    push rapide

--- includes/config_parser.hpp
+++ includes/config_parser.hpp
@@ -4,6 +4,31 @@
 
 # include <iostream>
 # include <fstream>
+# include <vector>
+# include <map>
+
+	// std::string tmp = "GET POST";
+	// if (tmp.find("POST") != (size_t)-1)
+	// 	std::cout << "find Post" << std::endl;
+	// if (tmp.find("GET") != (size_t)-1)
+	// 	std::cout << "find get" << std::endl;
+	// if (tmp.find("oseefikgheog") == (size_t)-1)
+	// 	std::cout << "find nothing" << std::endl;
+	// peut utiliser un truc dans le genre si pas vector
+
+typedef struct t_location {
+	std::string index;
+	std::vector<std::string> method;//check le mieux entre vector ou utiliser un find avec une string
+	std::vector<std::string> error;
+} s_location;
+
+typedef struct t_conf {
+	std::string port;
+	std::string name;
+	std::string root;
+	std::string autoindex = "off";
+	std::map<std::string, s_location> location;
+} s_conf;
 
 class config_parser
 {
